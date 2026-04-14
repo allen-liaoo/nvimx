5a5ca3fe72416c38842e8f8c6dbcff2745f0c7ab
Allen Liao <wcliaw610@gmail.com>
2026-04-13 22:17:57 -0500

    fix: move nixd flake inputs code into native vim, fix string escaping issues

--- nixvim/langs/nix.nix
+++ nixvim/langs/nix.nix
@@ -34,38 +34,58 @@
           '';
           default = { };
         };
-        inFlake = lib.mkOption {
-          type = lib.types.bool;
-          default = with config.nvimx.nix.nixd; 
-            builtins.any (s: s != "") [
-              nixpkgsName nixosConfKey hmConfKey
-            ] || flakeInputs != { };
-          readOnly = true;
-        };
       };
     };
   };
 
-  config = lib.mkIf (config.nvimx.nix.enable) {
+config = let
+  q = "\\\""; # nix's quote ("), escaped in lua (\"), escaped in nix
+  flakeExpr = "(builtins.getFlake ${q}\' .. find_flake_dir() .. \'${q})"; # see lsp.luaConfig below
+  # exprOfInput produces a nix expr in lua that gets the necessary path of an input
+  # for nixpkgs, this is the pkgs path
+  # for flake inputs (not including nixos or home-manager), this is the path to module options
+    # https://github.com/nix-community/nixd/blob/main/nixd/docs/configuration.md
+  exprOfInput = (input: path: "\'${flakeExpr}.inputs.${path}\'");
+in lib.mkIf (config.nvimx.nix.enable) {
     nvimx.lsp.enable = true;
 
     lsp.servers.nixd = {
       enable = true;
       activate = true;
-      config.nixd = {
-        diagnostic.suppress = [ "sema-extra-with" ];
+      config = {
+        cmd = [ "nixd" ]; # necessary
+        settings.nixd = with config.nvimx.nix.nixd; {
+          diagnostic.suppress = [ "sema-extra-with" ];
+
+          nixpkgs.expr = if nixpkgsName != "" 
+            then {
+              # need to use __raw because we want flake_dir in flakeExpr to be interpolated 
+              __raw =  "\'${flakeExpr}.inputs.${nixpkgsName}.legacyPackages.${system}\'";
+            }
+            else "import <nixpkgs> { }";
+
+          # flake inputs and default values
+          options = lib.mapAttrs
+            (_: path: {
+              expr.__raw = "\'${path}\'";
+            })
+            ( # input = full path (flake.full_path) to module options
+              lib.optionalAttrs (nixosConfKey != "") {
+                "nixos" = "${flakeExpr}.nixosConfigurations.${nixosConfKey}.options";
+              } // lib.optionalAttrs (hmConfKey != "") {
+                "[\"home-manager\"]" = "${flakeExpr}.homeConfigurations.${hmConfKey}.options";
+              } // lib.mapAttrs' (input: path: {
+                name = "[\"${input}\"]";
+                value = "${flakeExpr}.inputs.${input}.${path}.options";
+              }) flakeInputs
+            );
+        };
       };
     };
 
-    # tell nixd of flake inputs to enable lsp features
-    # Can't get it to work with config in native nixvim
-    lsp.luaConfig.post = with config.nvimx.nix.nixd; let
-      wrapInQuotes = s: s |> lib.splitString "." |> map (s': ''\"${s'}\"'') |> builtins.concatStringsSep ".";
-    in lib.optionalString inFlake ''
-      local lsp = vim.lsp
-
-      -- search up the path for flake directory
-      local function find_flake_dir()
+    lsp.luaConfig.pre = ''
+      -- search up the path for flake directory, falllback to cwd
+      function find_flake_dir()
         local dir = vim.fn.getcwd()
         while dir ~= "/" do
           if vim.fn.filereadable(dir .. "/flake.nix") == 1 then
@@ -73,49 +93,11 @@
           end
           dir = vim.fn.fnamemodify(dir, ":h")
         end
-    
-        return nil
+        return vim.fn.getcwd()
       end
-
-      local flake_dir = find_flake_dir()
-      if not flake_dir then
-        flake_dir = vim.fn.getcwd()
-      end
-      local flakeExpr = '(builtins.getFlake "' .. flake_dir .. '")'
-
-      lsp.config("nixd", {
-        settings = {
-          nixd = {
-            ${lib.optionalString (nixpkgsName != "") ''
-            nixpkgs = {
-              expr = flakeExpr .. ".inputs.\"${nixpkgsName}\".legacyPackages.\"${system}\"",
-            },'' }
-            options = {
-              ${
-              # would like to disable nixos options when using home manager but that is not possible; nixd will fallback to <nixpkgs>
-              # See: https://github.com/nix-community/nixd/issues/807
-              lib.optionalString (nixosConfKey != "") ''
-              nixos = {
-                expr = flakeExpr .. ".nixosConfigurations.\"${nixosConfKey}\".options",
-              },'' }
-              ${lib.optionalString (hmConfKey != "") ''
-              ["home-manager"] = {
-                expr = flakeExpr .. ".homeConfigurations.\"${hmConfKey}\".options",
-              },'' }
-              ${(lib.mapAttrsToList (input: modulePath: ''
-              ["${input}"] = {
-                expr = flakeExpr .. ".inputs.\"${input}\".${wrapInQuotes modulePath}.options",
-              },'') flakeInputs)
-                |> lib.concatStringsSep "\n" }
-            },
-          },
-        },
-      })
-      lsp.enable("nixd")
     '';
 
     nvimx.treesitter.enable = true;
-
     plugins.treesitter.grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
       nix
     ];
